59114f127ec744fc7955cd3947b0a45a99107deb
Brad Sickles <brad.sickles@gmail.com>
2025-12-18 17:56:14 -0500

    Remove cycle

--- app.tf
+++ app.tf
@@ -14,7 +14,7 @@ locals {
     // Inject app metadata into capabilities here (e.g. service_account_id)
     service_account_id    = google_service_account.app.id
     service_account_email = google_service_account.app.email
-    job_id                = google_cloud_run_v2_job.this.id
-    job_name              = google_cloud_run_v2_job.this.name
+    job_name              = local.job_name
+    job_id                = "projects/${local.project_id}/locations/${local.region}/jobs/${local.job_name}"
   })
 }
--- gcp.tf
+++ gcp.tf
@@ -1,9 +1,7 @@
 data "google_client_config" "this" {}
-data "google_client_openid_userinfo" "this" {}
 
 locals {
   region             = data.google_client_config.this.region
   zone               = data.google_client_config.this.zone
   project_id         = data.google_client_config.this.project
-  executing_sa_email = data.google_client_openid_userinfo.this.email
 }
